49b303f3cf50e46509388f03d5a643c2c3dc809d
Anton Sergeyev <sergeyev@kolesa.kz>
2023-12-02 11:04:05 +0600

    day 1 part 2

--- 2023/day-1/src/main.rs
+++ 2023/day-1/src/main.rs
@@ -1,17 +1,44 @@
-use std::fs;
 use std::env;
+use std::fs;
 
 fn main() {
-    let args: Vec<String>  = env::args().collect();
-    let path = args.get(1).expect("path is required: cargo run -- src/input.txt");
+    let args: Vec<String> = env::args().collect();
+    let path = args
+        .get(1)
+        .expect("path is required: cargo run -- src/input.txt");
     let readme = fs::read_to_string(path).expect("could not read file");
     println!("{}", get_text_value(readme.as_str()));
 }
 
-fn get_line_value(s: &str) -> u32 {
-    let digits: Vec<u32> = s.chars().filter_map(|c| c.to_digit(10)).collect();
+const WORDS2DIGITS: &[&str] = &[
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
 
-    digits.first().unwrap_or(&0) * 10 + digits.last().unwrap_or(&0)
+fn get_line_value(s: &str) -> u32 {
+    let bytes = s.as_bytes();
+    // all digits in a string are collected into a vec
+    // TODO may just look at the beginning and the end ignoring everything in between
+    let mut digits: Vec<u32> = Vec::new();
+
+    for (i, c) in bytes.iter().enumerate() {
+        if c.is_ascii_digit() {
+            digits.push((*c as char).to_digit(10).unwrap());
+        }
+
+        let word_position = WORDS2DIGITS.iter().position(|word| {
+            if i + word.len() > bytes.len() {
+                return false;
+            }
+
+            return word.as_bytes() == &bytes[i..(i+word.len())];
+        });
+        if let Some(word_index) = word_position {
+            digits.push(word_index as u32 + 1);
+        }
+    }
+
+    return digits.first().unwrap_or(&0) * 10 + 
+        digits.last().unwrap_or(&0);
 }
 
 fn get_text_value(s: &str) -> u32 {
@@ -29,6 +56,16 @@ mod tests {
         assert_eq!(99, get_line_value("asdfas9"));
         assert_eq!(0, get_line_value("asdfadsf"));
         assert_eq!(77, get_line_value("treb7uchet"));
+
+        assert_eq!(11, get_line_value("one"));
+        assert_eq!(12, get_line_value("onetwo"));
+
+        assert_eq!(24, get_line_value("xtwone3four"));
+        assert_eq!(29, get_line_value("two1nine"));
+        assert_eq!(83, get_line_value("8wo3"));
+        assert_eq!(83, get_line_value("eightwo3"));
+        assert_eq!(83, get_line_value("eightwothree"));
+        assert_eq!(42, get_line_value("4nineeightseven2"));
     }
 
     #[test]
